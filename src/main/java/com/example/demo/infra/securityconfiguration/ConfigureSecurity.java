0b6c5f035fd73260c8cdc132cf32c4367d55383a
EvErToN <evertonflorenciodasilva@outlook.com>
2024-08-05 09:53:44 -0300

    trabalhando no autenticador

--- src/main/java/com/example/demo/infra/securityconfiguration/ConfigureSecurity.java
+++ src/main/java/com/example/demo/infra/securityconfiguration/ConfigureSecurity.java
@@ -1,8 +1,12 @@
 package com.example.demo.infra.securityconfiguration;
 
+import java.net.Authenticator;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,8 +34,7 @@ public class ConfigureSecurity {
         return http.build();
     }
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
     }
-    
 }
